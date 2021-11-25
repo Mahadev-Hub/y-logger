829db87c7d7150bb0444f0bd1c76baa91a1d238d
Yogesh <yogeshdtu@gmail.com>
2021-11-25 07:41:58 -0600

    testing logger

---
+++ test_logger.py
@@ -0,0 +1,9 @@
+import logging
+from app.y_logger import logger
+
+if __name__ == '__main__':
+    logger.debug(f'[{logging.DEBUG}]: Test Message')
+    logger.info(f'[{logging.INFO}]: Test Message')
+    logger.warning(f'[{logging.WARNING}]: Test Message')
+    logger.error(f'[{logging.ERROR}]: Test Message')
+    logger.critical(f'[{logging.CRITICAL}]: Test Message')
